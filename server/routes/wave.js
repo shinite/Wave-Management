f2d517b0cd8394d534e7b3a1e0e699adb388ba1a
shinite <ssshambhavi8@gmail.com>
2016-09-23 10:48:08 +0000

    commit Delete

--- server/routes/wave.js
+++ server/routes/wave.js
@@ -3,6 +3,21 @@ var mongoose=require('mongoose')
 var waveObj=require('../model/wave');
 var app=express();
 
+
+var loggerTest=function(req,res,next){
+  waveObj.find(function(err,data){
+    if(err)
+    res.send(err);
+    else
+    {
+      console.log("successfull middleware");
+      console.log(data);
+      res.send(data);
+    }
+  });
+}
+
+
 app.post('/add',function(req,res){
   console.log("Inside Add");
   var wave =new waveObj(req.body);
@@ -35,6 +50,23 @@ app.get('/display',function(req,res,next){
 }
 );
 
+app.delete('/delete/:wave',function(req,res,next){
+  console.log(req.params.wave);
+  //var movie=mongoose.model("MovieDetails");
+  waveObj.findOneAndRemove({waveName: req.params.wave}, function (err,offer){
+    if(err) { res.send(err);
 
+      console.log(err);
+     }
+    else{
+      // console.log("successfully deleted");
+      //res.send("successfully deleted");
+      next();
+    }
+  })
+});
+
+
+app.use(loggerTest);
 
 module.exports=app;
